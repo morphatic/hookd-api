536649eb24d4b29b7c6ee3a2134b4d4ff1bc2e54
Morgan Benton <morgan.benton@gmail.com>
2020-11-16 12:59:19 -0500

    added users service

---
+++ src/models/users.model.js
@@ -0,0 +1,45 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const users = sequelizeClient.define('users', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false, // this means field is required
+      unique: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    first_name: {
+      type: DataTypes.STRING
+    },
+    last_name: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    phone: {
+      type: DataTypes.STRING
+    },
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  users.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return users;
+};
